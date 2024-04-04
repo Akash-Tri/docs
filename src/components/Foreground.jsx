4dccf3bf471108ed7ace8a7f4e966e7fa821c7b5
AKASH TRIPATHI <tripathig0542@gmail.com>
2024-04-04 11:58:25 +0530

    This is first commit

---
+++ src/components/Foreground.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import Card from "./Card";
+import { useRef } from "react";
+
+function Foreground() {
+
+  const ref = useRef(null);
+  
+  const data = [
+    {
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+      filesize: ".9mb",
+      close: false,
+      tag: { isOpen: true, tagTitle: "Download Now", tagColor: "green" },
+    },
+    {
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+      filesize: ".9mb",
+      close: false,
+      tag: { isOpen: true, tagTitle: "Download Now", tagColor: "blue" },
+    },
+    {
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+      filesize: ".9mb",
+      close: false,
+      tag: { isOpen: true, tagTitle: "Download Now", tagColor: "green" },
+    },
+  ];
+  return (
+    <div ref={ref} className="fixed top-0 left-0 z-[3] w-full h-full flex gap-5 p-5 flex-wrap">
+      {data.map((item, index) => (
+        <Card data={item} reference={ref}/>
+      ))}
+    </div>
+  );
+}
+
+export default Foreground;
